527f7ced23ab686f26392db78489ece77605e291
Janusz Slawek <janusz.slawek@gmail.com>
2014-04-03 00:37:52 -0400

    Create example.m

---
+++ example.m
@@ -0,0 +1,17 @@
+% set default parameters
+C=30;
+T=10;
+Psi=0.25;
+Xi=0.75;
+S=0.67;
+delta=0.05;
+
+% create random data table
+
+path(path,'src');
+
+RandomData = normrnd(0,1,10,10);
+
+V=adanet(RandomData,1:10,C,T,Psi,Xi,S,delta);
+
+% you can apply post-processing steps here
