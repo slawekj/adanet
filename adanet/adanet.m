fa3e3793ff2a8e65e8544450c0eedbad34b827cc
Janusz Slawek <janusz.slawek@gmail.com>
2014-04-03 01:28:37 -0400

    Update adanet.m

--- adanet/adanet.m
+++ adanet/adanet.m
@@ -1,21 +1,20 @@
-
- %   This is an implementation of ADANET algorithm for Gene Regulatory Network
- %   inference from mRNA expression data, in form of a Matlab package.
- %   Copyright (C) 2014  Janusz Slawek
- %
- %   This program is free software: you can redistribute it and/or modify
- %   it under the terms of the GNU General Public License as published by
- %   the Free Software Foundation, either version 3 of the License, or
- %   any later version.
- %
- %   This program is distributed in the hope that it will be useful,
- %   but WITHOUT ANY WARRANTY; without even the implied warranty of
- %   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- %   GNU General Public License for more details.
- %
- %   You should have received a copy of the GNU General Public License
- %   along with this program, see LICENSE.
- %
+%   This is an implementation of ADANET algorithm for Gene Regulatory Network
+%   inference from mRNA expression data, in form of a Matlab package.
+%   Copyright (C) 2014  Janusz Slawek
+%
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this program, see LICENSE.
+%
 
 function V = adanet(E,regulatorsI,T,t,Psi,Xi,s,delta)
 
